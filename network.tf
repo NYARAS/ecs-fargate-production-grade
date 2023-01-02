f1862d5dbc7e29caf7c2aac48c8ad026b11af87e
Calvine Otieno <nyarangaotieno@gmail.com>
2023-01-02 12:06:06 +0300

    added private subnets
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- network.tf
+++ network.tf
@@ -152,3 +152,80 @@ resource "aws_nat_gateway" "public_b" {
     })
   )
 }
+
+#################################################
+# Private Subnets - Outbound Internet Access only
+#################################################
+resource "aws_subnet" "private_a" {
+  cidr_block        = "10.1.10.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    tomap(
+      {
+        "Name" = "${local.prefix}-private-a"
+    })
+  )
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap(
+      {
+        "Name" = "${local.prefix}-private-a"
+    })
+  )
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route" "private_a_internet_out" {
+  route_table_id         = aws_route_table.private_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.public_a.id
+}
+
+resource "aws_subnet" "private_b" {
+  cidr_block        = "10.1.11.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = merge(
+    local.common_tags,
+    tomap(
+      {
+        "Name" = "${local.prefix}-private-b"
+    })
+  )
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap(
+      {
+        "Name" = "${local.prefix}-private-b"
+    })
+  )
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+
+resource "aws_route" "private_b_internet_out" {
+  route_table_id         = aws_route_table.private_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.public_b.id
+}
