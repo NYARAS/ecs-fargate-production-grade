0def7882e9e16499b5c5d0bbfe174dfa04f25ab0
Calvine Otieno <nyarangaotieno@gmail.com>
2023-01-04 22:35:33 +0200

    Added ecs roles
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- ecs.tf
+++ ecs.tf
@@ -3,3 +3,29 @@ resource "aws_ecs_cluster" "main" {
 
   tags = local.common_tags
 }
+
+resource "aws_iam_policy" "task_execution_role_policy" {
+  name        = "${local.prefix}-task-exec-role-policy"
+  path        = "/"
+  description = "Allow retrieving of images and adding to logs"
+  policy      = file("./templates/ecs/task-exec-role.json")
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${local.prefix}-task-exec-role"
+  assume_role_policy = file("./templates/ecs/assume-role-policy.json")
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.task_execution_role_policy.arn
+}
+
+resource "aws_iam_role" "app_iam_role" {
+  name               = "${local.prefix}-api-task"
+  assume_role_policy = file("./templates/ecs/assume-role-policy.json")
+
+  tags = local.common_tags
+}
